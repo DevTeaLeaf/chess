00840b9dca1c858a50cd5fa979be29459177970c
DevTeaLeaf <107722213+DevTeaLeaf@users.noreply.github.com>
2022-07-15 02:34:16 +0300

    add cursor

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -10,12 +10,25 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    setSelectedCell(cell);
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
           {row.map((cell) => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              click={click}
+              cell={cell}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+              }
+            />
           ))}
         </React.Fragment>
       ))}
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,13 +1,19 @@
+import { click } from "@testing-library/user-event/dist/click";
 import React, { FC } from "react";
 import { Cell } from "../models/Cell";
 
 interface CellProps {
   cell: Cell;
+  selected: boolean;
+  click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected, click }) => {
   return (
-    <div className={["cell", cell.color].join(" ")}>
+    <div
+      className={["cell", cell.color, selected ? "selected" : ""].join(" ")}
+      onClick={() => click(cell)}
+    >
       {cell.figure?.logo && <img src={cell.figure.logo} alt="" />}
     </div>
   );
